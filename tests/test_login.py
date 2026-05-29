729224546145a3a2dff6f55907d377d829b26abf
Sumant Mishra <sumantmishra075@gmail.com>
2026-05-29 15:00:32 +0530

    Fixed headless Chrome issue in CI/CD

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,28 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.chrome.options import Options
 
 def test_login():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    wait = WebDriverWait(driver, 10)
 
-    try:
-        driver.get("https://www.saucedemo.com/")
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-        # login
-        wait.until(EC.presence_of_element_located((By.ID, "user-name"))).send_keys("standard_user")
-        driver.find_element(By.ID, "password").send_keys("secret_sauce")
-        driver.find_element(By.ID, "login-button").click()
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
 
-        # validation
-        inventory = wait.until(
-            EC.presence_of_element_located((By.CLASS_NAME, "inventory_list"))
-        )
+    driver.get("https://www.saucedemo.com/")
 
-        assert inventory.is_displayed()
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
 
-    finally:
-        driver.quit()
+    assert "inventory" in driver.current_url
+
+    driver.quit()
